d2886aec4a26d15e17ffb2f8723a75ddccee0f65
Cotton Malone <ctnmalone@gmail.com>
2023-12-11 00:04:23 +0100

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -13,15 +13,16 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "armv7l-linux" "aarch64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        qjs_version = "2023-12-09_1";
         arch =
           if system == "x86_64-linux" then "x86_64"
           else if system == "armv7l-linux" then "armv7l"
           else if system == "aarch64-linux" then "aarch64"
           else "unknown-arch";
         sha256 =
-          if system == "x86_64-linux" then "sha256:125a4q49gqyma10d12syxa90blcdyx5rfky69xmcpkwn6q8842yr"
-          else if system == "armv7l-linux" then "sha256:1amn4d46cml3y8iq9a35aai59fqrjbnsik1vpc7z5n0hybs91bf9"
-          else if system == "aarch64-linux" then "sha256:1rghzvqi6rjf402zg2bf94x1rnw6qc673cmdnpdcsk6ma2skqsi9"
+          if system == "x86_64-linux" then "sha256:0jzz6y0da2b7bi6k0l7qf7kl2d9j2lhvsighfxg3qsb1z20y3rgd"
+          else if system == "armv7l-linux" then "sha256:1drd4ail0qs3lj5gxja664iggbk0ii40layd6g6aq13zl2px4vq6"
+          else if system == "aarch64-linux" then "sha256:0zi4l8hqcj4y3y66lw4zcn19a9d6bcgp8y3pxmikrznmrxjqgmlr"
           else "sha256:0000000000000000000000000000000000000000000000000000";
       in
       {
@@ -30,7 +31,7 @@
           name = "quickjs-static";
 
           src = builtins.fetchTarball {
-            url = "https://github.com/ctn-malone/quickjs-cross-compiler/releases/download/2021-03-27_4/quickjs.core.2021-03-27_4.${arch}.tar.xz";
+            url = "https://github.com/ctn-malone/quickjs-cross-compiler/releases/download/v${qjs_version}/quickjs.core.${qjs_version}.${arch}.tar.xz";
             sha256 = sha256;
           };
 
