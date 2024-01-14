c7abda0bac4e99651cedc6ae74b418efd79e428d
Cotton Malone <ctnmalone@gmail.com>
2024-01-14 01:38:47 +0100

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -13,16 +13,16 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "armv7l-linux" "aarch64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        qjs_version = "2023-12-09_1";
+        qjs_version = "2024-01-13_1";
         arch =
           if system == "x86_64-linux" then "x86_64"
           else if system == "armv7l-linux" then "armv7l"
           else if system == "aarch64-linux" then "aarch64"
           else "unknown-arch";
         sha256 =
-          if system == "x86_64-linux" then "sha256:0jzz6y0da2b7bi6k0l7qf7kl2d9j2lhvsighfxg3qsb1z20y3rgd"
-          else if system == "armv7l-linux" then "sha256:1drd4ail0qs3lj5gxja664iggbk0ii40layd6g6aq13zl2px4vq6"
-          else if system == "aarch64-linux" then "sha256:0zi4l8hqcj4y3y66lw4zcn19a9d6bcgp8y3pxmikrznmrxjqgmlr"
+          if system == "x86_64-linux" then "sha256:00rk22na9vlfn3jpj2wjqhaqxz8r6j7wp5nr2pn84nwziy7xvkf5"
+          else if system == "armv7l-linux" then "sha256:0wk3n8rmnpnjpgn2ab375d7halr6cxi8fh6bvvzahq27pbf79yy5"
+          else if system == "aarch64-linux" then "sha256:139lfvdfbjix0z6szsq0nmmx3jvbgj56amqlg877830af2vhmr3g"
           else "sha256:0000000000000000000000000000000000000000000000000000";
       in
       {
