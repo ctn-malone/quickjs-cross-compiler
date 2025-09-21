1066cf6aad9f10edfafc4e0302d7d951a327f437
Cotton Malone <ctnmalone@gmail.com>
2025-09-21 13:16:58 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -18,16 +18,16 @@
 
         highlight = text: "\\x1b[1;38;5;212m${text}\\x1b[0m";
 
-        qjs_version = "2025-09-13_2";
+        qjs_version = "2025-09-13_3";
         arch =
           if system == "x86_64-linux" then "x86_64"
           else if system == "armv7l-linux" then "armv7l"
           else if system == "aarch64-linux" then "aarch64"
           else "unknown-arch";
         sha256 =
-          if system == "x86_64-linux" then "sha256:1wsx0pq8cl38x02arjdm419dm997lx5ccnzqqxvwpzsszihl11ms"
-          else if system == "armv7l-linux" then "sha256:1f1aq3xxd3768fwjhz6wn3a8qh2cj8qcg750bgazq8h6xdv4hixw"
-          else if system == "aarch64-linux" then "sha256:0gg2wvmq81qqxwsk6x2l5209xkylwwf936imr3s7jrv98fq5bil1"
+          if system == "x86_64-linux" then "sha256:0kkpmsi27bscy151g0gsax2l1hx1y5xc35pq5vgj7vvmmaj0nqd7"
+          else if system == "armv7l-linux" then "sha256:1bckbkrwlhhiq96l56wrjlq11nr87gmvzp0wixk3ynslip60dv1l"
+          else if system == "aarch64-linux" then "sha256:1533h4jx3b9pisaj3853fsgwkvgip7h4xljl6yhbimxi121qzswb"
           else "sha256:0000000000000000000000000000000000000000000000000000";
 
         quickjsStatic = pkgs.stdenv.mkDerivation {
